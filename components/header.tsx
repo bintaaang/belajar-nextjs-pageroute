79cd7ae425eba941694dae36a6ae99f62c61c82a
bintang <akuntugasdkk@gmail.com>
2025-08-05 17:53:56 +0700

    day 2

---
+++ components/header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="text-emerald-300 bg-white flex flex-row gap-x-4 py-4">
+      <h1>My Website</h1>
+      <h1>wkwkwkwkwkwk</h1>
+    </header>
+  );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Header from '@/components/header';
+import Footer from '@/components/footer';
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -13,8 +15,10 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
+    <>
+    <Header />
     <div
-      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
+      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[auto_1fr_auto] min-h-screen items-center justify-items-center`}
     >
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
@@ -62,54 +66,9 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+        <Footer />
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.orrg →
-        </a>
-      </footer>
     </div>
+    </>
   );
 }
